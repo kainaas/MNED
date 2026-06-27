b90736ae8b5404d45cf21cb717cfe94985ab4b5f
viniciusDSF-USP <desaferreirav@usp.br>
2026-06-27 13:36:01 -0300

    First discretization graphs; second discretization done and von neumman done

--- T_3/main.py
+++ T_3/main.py
@@ -5,33 +5,46 @@ from scipy.sparse import lil_matrix
 # Consts
 eps = 0.1
 P = [0.1, 1, 20]
-N = 140 # Number of points in x (temporary for tests, 'till 140 takes less than 3s)
+N = 430 # Number of points in x ('till 140 takes less than 3s)
 Dx = 15/(N-1)
 Nx = int(np.floor(15/Dx)) + 1
+
+animate = False
+show = False
+save = True
+feedback = True
 
 # Cases (I), (II) e (III) for first discretization
 for k,Pe in enumerate(P):
     Dt = np.min([ Dx**2 / (2/Pe + Dx), Dx ])*(1-eps)
     Nt = int(np.floor(12/Dt)) + 1
+    #print(f'Dx={Dx:.4e}, Dt={Dt:.4e}')
 
     xj = np.arange(0,Nx,1)*Dx
     tn = np.arange(0,Nt,1)*Dt
     U0 = np.exp(-20*(xj-2)**2) + np.exp(-(xj-5)**2)
 
     # Initial condition
-    if k == 0:
-        fig = plt.figure(figsize=(10,5))
-        initial = fig.add_subplot(111)
+    if show or save:
+        if k == 0:
+            fig = plt.figure(figsize=(9,5))
+            initial = fig.add_subplot(111)
 
-        x = np.linspace(0,15,1000)
-        u0 = np.exp(-20*(x-2)**2) + np.exp(-(x-5)**2)
-        initial.plot(x, u0, '-k')
-        initial.plot(xj, U0, '.r', zorder=2)
+            x = np.linspace(0,15,1000)
+            u0 = np.exp(-20*(x-2)**2) + np.exp(-(x-5)**2)
+            initial.plot(x, u0, '-k', label=r'$u(x,0)$')
+            initial.plot(xj, U0, '.r', zorder=2, label=r'$U(x_j,0)$')
 
-        initial.set_xticks(np.arange(0,16))
+            initial.set_xticks(np.arange(0,16))
+            initial.set_xlabel('x')
+            initial.set_ylabel('u')
+            initial.grid(True)
+            initial.legend()
 
-        plt.tight_layout()
-        plt.show()
+            plt.tight_layout()
+            if show: plt.show()
+            if save: plt.savefig('graphs/initial_condition.pdf', bbox_inches='tight', format='pdf')
+            exit(0)
 
     # First discretization
     m = Nx
@@ -63,18 +76,48 @@ for k,Pe in enumerate(P):
     U = np.zeros((Nt, Nx))
     U[0, :] = U0
 
+    if feedback: print(f'Calculating Pe={Pe}...', end='', flush=True)
     for n in range(1, Nt):
         U[n, :] = A.dot(U[n-1, :])
+    if feedback and (show or save): print(' Ploting...', end='', flush=True)
 
-    fig = plt.figure(figsize=(10,5))
-    final = fig.add_subplot(111, projection='3d')
-    final.plot_surface(X,T,U,cmap='plasma')
+    if show or save:
+        fig = plt.figure(figsize=(9,5))
+        final = fig.add_subplot(111, projection='3d')
 
-    final.set_xlabel('x')
-    final.set_ylabel('t')
-    final.set_zlabel(r'$U$')
+        surf = final.plot_surface(X,T,U,cmap='plasma')
+        fig.colorbar(surf, ax=final, shrink=0.8, pad=0.1)
 
-    final.set_title(r'$\mathbb{P}e = '+f'{Pe}'+r'$')
+        final.set_xlabel('x')
+        final.set_ylabel('t')
+        final.set_zlabel(r'$U$')
+        final.grid(True)
 
-    plt.tight_layout()
-    plt.show()
+        if show: final.set_title(r'$\mathbb{P}e = '+f'{Pe}'+r'$')
+
+        plt.tight_layout()
+        if show: plt.show()
+        # For Pe << 1 and Pe = 1, pdf gets too big, so used png
+        if save: plt.savefig(f'graphs/surface_Pe={Pe}.png', bbox_inches='tight', format='png', dpi=300)
+    if feedback: print(' Done!')
+
+    if animate:
+        plt.close('all')
+
+        fig = plt.figure(figsize=(10,5))
+        evol = fig.add_subplot(111)
+
+        n = U.shape[0]
+        it = 100
+        for k in range(n):
+            if not plt.fignum_exists(fig.number):
+                plt.close('all')
+                break
+
+            if k%int(n/it) == 0 or k+1 == n:
+                evol.cla()
+                evol.plot(X[k,:],U[k,:])
+                evol.set_title(r'$\mathbb{P}e = '+f'{Pe}'+r',\ t='+f'{k}/{n-1}'+'$')
+                plt.pause(1) if k == 0 else plt.pause(1/it)
+    
+    plt.close('all')
